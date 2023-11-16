90869b6e0e7ffa9abe1ed395dc11e2525283c917
Diego Coleto Checa <diegocoleto7@gmail.com>
2023-11-16 01:22:07 +0100

    Fixed typing errors

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,6 +1,6 @@
-mkdi -p res/genome
+mkdir -p res/genome
 wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-gunzip res/genome/ecoli.fasta.gz
+gunzip -k res/genome/ecoli.fasta.gz
  echo "Running STAR index..."
     mkdir -p res/genome/star_index
     STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
